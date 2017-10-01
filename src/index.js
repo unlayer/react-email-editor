639e2174fb5c15e3d8354a10c8cc59e69c62ece0
Umair Siddique <umairsiddique@gmail.com>
2017-10-01 19:30:54 -0400

    Created initial version of the component.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,50 @@
 import React, {Component} from 'react'
+import Script from 'react-load-script'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  flex: 1;
+  display: flex;
+`
+
+const Designer = styled.div`
+  flex: 1;
+  display: flex;
+
+  > iframe {
+    flex: 1;
+    width: 100%;
+    height: 100%;
+    min-height: ${props => props.minHeight || '700px'} !important;
+    display: flex;
+    border: 0px;
+  }
+`
 
 export default class extends Component {
   render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+    return (
+      <Wrapper>
+        <Script
+          url="https://designer.unroll.io/assets/embed.bundle.js"
+          onLoad={this.unrollReady}
+        />
+
+        <Designer
+          id="editor"
+          style={this.props.style}
+          minHeight={this.props.minHeight}
+        />
+      </Wrapper>
+    )
+  }
+
+  unrollReady = () => {
+    unroll.init({
+      id: 'editor',
+      displayMode: 'email',
+      projectId: this.props.projectId,
+      templateId: this.props.templateId,
+    })
   }
 }
